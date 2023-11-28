c5e7f012bd45a2b0a8d21d06f76f1d774a82ef8b
4llves <jhonatab4@gmail.com>
2023-11-27 23:46:44 -0300

    persistindo bando de dados

--- src/database.js
+++ src/database.js
@@ -1,5 +1,35 @@
+import fs from 'node:fs'
+
+const databasePath = new URL('../db.json', import.meta.url)
+
 export class Database {
   #database = {}
+
+  // constructor() {
+  //   fs.readFile(databasePath, 'utf8')
+  //     .then(data => {
+  //         this.#database = JSON.parse(data)
+  //     })
+  //     .catch (() => {
+  //         this.#persist()
+  //     })
+  // } 
+
+  constructor() {
+    try {
+      const data = fs.readFileSync(databasePath, 'utf8');
+      this.#database = JSON.parse(data);
+    } catch (error) {
+      this.#persist();
+    }
+  }
+
+  #persist() {
+    fs.writeFile(
+      databasePath, JSON.stringify(this.#database),
+      (e) => { console.log(e)}
+    )
+  }
 
   select(table) {
     const data = this.#database[table] ?? []
@@ -13,6 +43,8 @@ export class Database {
     } else { 
       this.#database[table] = [data]
     }
+
+    this.#persist()
 
     return data
   }
