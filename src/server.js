398b8f918309b3eac189b41bf811df6403a0281f
4llves <jhonatab4@gmail.com>
2023-11-29 23:06:54 -0300

    Route e Query Parameters

--- src/server.js
+++ src/server.js
@@ -1,6 +1,16 @@
 import http from 'node:http'
+
 import { json } from './middlewares/json.js'
 import { routes } from './routes.js'
+
+
+// Query Parameters: URL Stateful => Filtros, Paginação, Não-Obritarórios
+// http://localhost:3333/users?userId=1&name=alves
+// Route Parameters: 
+// GET http://localhost:3333/users/1 "buscar user com id 1"
+// Request Body: 
+// Envio de informações de um forme e afins
+
 
 const server = http.createServer(async (req, res) => {
   const { method, url } = req
